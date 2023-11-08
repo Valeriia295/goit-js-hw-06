6852711b6f4188e83c77a0e1b1aab1fc8a87dd1b
Valeriia295 <sokolsokol295@gmail.com>
2023-11-08 23:41:39 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,11 @@
-const registerForm = document.querySelector("form.login-form");
+const registerForm = document.querySelector(".login-form");
 
 registerForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const login = form.elements.email.value;
+  const email = form.elements.email.value;
   const password = form.elements.password.value;
 
   if (email === "" || password === "") {
